4b20a97964e7249e482b40bfd1de8f7784913591
raisavar <raisavar@users.noreply.github.com>
2025-12-17 15:21:29 +0200

    refactor: code cleanup

--- src/api.js
+++ src/api.js
@@ -185,3 +185,30 @@ class API {
 
 // Update 43
 module.exports = API;
+
+
+// API module for AdventureEngine
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 49
+module.exports = API;
